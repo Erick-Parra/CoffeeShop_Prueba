325e38f29cad6c018dc8e85d99893fee8141a001
Erick-Parra <erick.parra.matarrita@est.una.ac.cr>
2022-05-16 21:26:00 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require("express")
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+//Produts
+app.post('/createProduct', async(req, res)=> {
+  const {name, image, price } =  req.body
+  const result = await prisma.product.create({
+      data:{
+          name, image, price
+      }
+  })
+  res.json(result)
+})
+
+
+app.get('/products', async(req, res) =>{
+  const products = await prisma.product.findMany()
+  res.json(products)
+})
+
+
+app.put('/product/:id', async(req, res)=>{
+  const {id} = req.params
+  const {name, description, price, image} = req.body
+  const product = await prisma.product.update({
+    where:{id: Number(id)},
+    data:{name, description, price, image}
+  })
+  res.json(product)
+})
+
+
+app.delete('/product/:id', async(req, res)=>{
+  const {id} = req.params
+  const product = await prisma.product.delete({
+    where: {id: Number(id)}
+  })
+  res.json(`Producto eliminado`)
+})
+
+//Orders
+app.post('/createOrder', async(req, res)=> {
+    const {user, items} = req.body
+    try {
+        const orderCreated = await prisma.order.create({
+            data: {
+                user,
+                items: {
+                    create: items
+                }
+            },
+            include: {
+                items: true
+            }
+        })
+        res.json(orderCreated)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
+
+app.get('/orders', async(req, res) =>{
+    const orders = await prisma.order.findMany({include:{items:true}})
+    res.json(orders)
+  })
+
+
+app.put('/order/:id', async(req, res)=>{
+    const {id} = req.params
+    const {status} = req.body
+    const order = await prisma.order.update({
+      where:{id: Number(id)},
+      data:{status}
+    })
+    res.json(order)
+  })
+
+app.listen(3000, ()=>
+console.log(`El server está listo`)
+)
---
+++ prisma/migrations/20220517032043_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `image` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Item` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `productId` INTEGER NOT NULL,
+    `qty` INTEGER NOT NULL,
+    `orderId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+    `user` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Item` ADD CONSTRAINT `Item_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
